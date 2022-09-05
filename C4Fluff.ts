4b6e86a5e8a5250638633ad020c99c9286b1acf2
vardevvas <vardevvas@gmail.com>
2022-09-05 09:22:57 +0200

    chp4

---
+++ C4Fluff.ts
@@ -0,0 +1,28 @@
+import { JOURNAL } from `/Users/devas/development/Elquent JS/journal.js`;
+
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
+function journalEvents(journal) {
+    let events = [];
+    for (let entry of journal) {
+        for (let event of entry.events) {
+            if (!events.includes(event)) {
+                events.push(event);
+            }
+        }
+    }
+    return events;
+}
+
+console.log(journalEvents(JOURNAL));
+for (let event of journalEvents(JOURNAL)) {
+    console.log(event + ":", phi(tableFor(event, JOURNAL)));
+}
--- Scrapping.ts
+++ Scrapping.ts
@@ -1,4 +1,14 @@
-//# Tringle
-for (let s = ""; s.length <= 7; s += "#") {
-    console.log(s);
+function countB(x) {
+    let bees = 0;
+    let notBee = 0;
+    for (let counter = 0; counter < x.lenght - 1; counter++) {
+        if (x[counter] == "B") {
+            bees += 1;
+        }
+        if (x[counter] != "B") {
+            notBee += 1;
+        }
+    }
+    return `there are ${bees} of Bs in this string and the rest is ${notBee}`;
 }
+console.log(countB("Strange ! Still don't know my names s S"))
