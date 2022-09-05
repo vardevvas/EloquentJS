302dd50b727ddcf92dfb3e0b1178d5aeaf02f505
vardevvas <vardevvas@gmail.com>
2022-09-05 09:39:36 +0200

    again

--- C4Fluff.ts
+++ C4Fluff.ts
@@ -1,14 +1,10 @@
-import { JOURNAL } from `/Users/devas/development/Elquent JS/journal.js`;
-
-function tableFor(event, journal) {
-    let table = [0, 0, 0, 0];
-    for (let i = 0; i < journal.length; i++) {
-        let entry = journal[i], index = 0;
-        if (entry.events.includes(event)) index += 1;
-        if (entry.squirrel) index += 2;
-        table[index] += 1;
-    }
-    return table;
+let journal = `require('/Users/devas/development/Elquent JS/journal.js')`
+function phi(table) {
+    return (table[3] * table[0] - table[2] * table[1]) /
+        Math.sqrt((table[2] + table[3]) *
+            (table[0] + table[1]) *
+            (table[1] + table[3]) *
+            (table[0] + table[2]));
 }
 function journalEvents(journal) {
     let events = [];
@@ -21,8 +17,16 @@ function journalEvents(journal) {
     }
     return events;
 }
-
-console.log(journalEvents(JOURNAL));
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
 for (let event of journalEvents(JOURNAL)) {
     console.log(event + ":", phi(tableFor(event, JOURNAL)));
 }
