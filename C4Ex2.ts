e4205e66711e9d0169a2e405ca7b059b2aca2555
vardevvas <vardevvas@gmail.com>
2022-09-06 10:02:03 +0200

    C4Ex2

---
+++ C4Ex2.ts
@@ -0,0 +1,20 @@
+function reverse(anArray) {
+    let reversedArray = []
+    for (let elemnt of anArray) {
+        reversedArray.unshift(elemnt)
+    }
+    return reversedArray
+}
+function reverseElements(elemntArray) {
+    let elemnto = []
+    for (let elemnt of elemntArray) {
+        let parameterss = []
+        for (let theElement of elemnt) {
+            parameterss.unshift(theElement)
+        }
+
+        elemnto.push(parameterss)
+    }
+    return elemnto
+}
+console.log(reverseElements(["cat", "dog", "house"]))
