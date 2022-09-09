addc15392262af0ac6e1e8076727b5ab4fd17d3a
Devas <vardevvas@gmail.com>
2022-09-09 20:14:40 +0200

    DONE EX 3

--- C4Ex3.ts
+++ C4Ex3.ts
@@ -13,9 +13,28 @@ function arrayToList(anArray) {
     }
 }
 function listToArray(aList) {
-    for (let anArray = aList; anArray; anArray = aList.rest) {
-        anArray.push(aList.value)
+    let newArray = []
+    while (aList != null) {
+        newArray.push(aList.value)
+        aList = aList.rest
+    }
+    return newArray
+}
+function prepend(element, list) {
+    let theList = {}
+    theList.value = element
+    theList.rest = list
+    return theList
+}
+function nth(list, position) {
+    let x = 0
+    if (x == position) {
+        return list.value
+    }
+    else {
+        return nth(list.rest, position - 1)
     }
 }
-console.log(arrayToList([1, 2, 3]))
-console.log(listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+console.log(prepend(10, prepend(20, null)));
+console.log(listToArray(arrayToList([1, 2, 3])))
+console.log(nth(arrayToList([1, 2, 3]), 1))
